c7bf2ea38b62c4a3e93fab26d9139dd1f441eb4b
mpawelm <mozola.pawel@gmail.com>
2019-03-04 15:34:40 +0100

    Add Team to Division

--- src/Client/src/app/settings/teams/teams.component.ts
+++ src/Client/src/app/settings/teams/teams.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DivisionService } from '../../shared/services/division.service';
 import { Observable } from 'rxjs';
 import { IDivision } from '../../shared/models/division.viewmodel';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-teams',
@@ -11,9 +12,46 @@ import { IDivision } from '../../shared/models/division.viewmodel';
 export class TeamsComponent implements OnInit {
   divisions: Observable<IDivision[]>;
 
-  constructor(private divisionService: DivisionService) { }
+  divisionsFormGroup: FormGroup;
+
+  constructor(private divisionService: DivisionService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.divisions = this.divisionService.getDivisions();
+
+    this.divisionsFormGroup = this.formBuilder.group({
+      seasonId: [''],
+      divisions: this.formBuilder.array([])
+    });
+
+    const divisionsFormArray = <FormArray>this.divisionsFormGroup.controls['divisions'];
+
+    this.divisions.subscribe(x => x.forEach(division => {
+      divisionsFormArray.push(this.formBuilder.group({
+        divisionId: [division.id],
+        teams: this.formBuilder.array([])
+      }));
+    }));
+  }
+
+  teamsformArray(id: string): FormArray {
+    const divi = this.divisionsFormGroup.controls['divisions'] as FormArray;
+    const divisionForm = divi.controls.filter(x => x.get('divisionId').value === id)[0] as FormGroup;
+
+    return <FormArray>divisionForm.controls['teams'];
+  }
+
+  addNewTeam(id: string) {
+    const divi = this.divisionsFormGroup.controls['divisions'] as FormArray;
+
+    const divisionForm = divi.controls.filter(x => x.get('divisionId').value === id)[0] as FormGroup;
+    const teams = <FormArray>divisionForm.controls['teams'];
+    teams.controls.push(this.formBuilder.group({
+      name: 'fakeName',
+      email: ''
+    }));
+  }
+
+  onSubmit() {
   }
 }
