ac8cd44dbcb0503e66c832509498c3ccf8f038f9
mpawelm <mozola.pawel@gmail.com>
2019-03-14 07:41:04 +0100

    Get next matches query

--- src/API/FL.API/Queries/QueryHandlers/GetNextMatchesQueryHandler.cs
+++ src/API/FL.API/Queries/QueryHandlers/GetNextMatchesQueryHandler.cs
@@ -22,12 +22,30 @@ namespace FL.API.Queries.QueryHandlers
 
         public async Task<IList<FixtureMatchViewModel>> Handle(GetCurrentFixtureQuery request, CancellationToken cancellationToken)
         {
+            var fixture = this.GetCurrentOrNextFixture();
+
             var query = this.dbContext.FixtureMatches
-                .Where(x => x.StartDate.Date >= DateTime.Now.Date && x.EndDate.Date <= DateTime.Now.Date);
+                .Where(x => x.FixtureId == fixture.FixtureId)
+                .Where(x => !x.Posponed);
 
                 return request.Number.HasValue ?
                     await query.Take(request.Number.Value).ToListAsync(cancellationToken) :
                     await query.ToListAsync(cancellationToken);
+        }
+
+        private FixtureViewModel GetCurrentOrNextFixture()
+        {
+            var fixture = this.dbContext.Fixtures.FirstOrDefault(x => x.StartDate < DateTime.Now && x.EndDate > DateTime.Now);
+
+            if (fixture == null)
+            {
+                return this.dbContext.Fixtures
+                    .Where(x => x.StartDate > DateTime.Now)
+                    .OrderBy(x => x.StartDate)
+                    .FirstOrDefault();
+            }
+
+            return fixture;
         }
     }
 
