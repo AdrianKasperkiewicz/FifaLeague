631aa307d57e1858394de3c81e4fc8899c06633d
mpawelm <mozola.pawel@gmail.com>
2019-03-17 19:19:03 +0100

    add flex

--- src/Client/src/app/matches/matches.module.ts
+++ src/Client/src/app/matches/matches.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { MatchesRoutingModule } from './matches-routing.module';
 import { AddScoreComponent } from './pages/add-score/add-score.component';
 import { MaterialModule } from '../shared/modules/material-module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [AddScoreComponent],
   imports: [
     CommonModule,
     MatchesRoutingModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ]
 })
 export class MatchesModule { }
