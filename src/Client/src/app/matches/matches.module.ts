6b2b66f79593da421816826ce30ba95202886a01
mpawelm <mozola.pawel@gmail.com>
2019-04-27 14:24:56 +0200

    Revert "add flex"
    This reverts commit 631aa307d57e1858394de3c81e4fc8899c06633d.

--- src/Client/src/app/matches/matches.module.ts
+++ src/Client/src/app/matches/matches.module.ts
@@ -4,15 +4,13 @@ import { CommonModule } from '@angular/common';
 import { MatchesRoutingModule } from './matches-routing.module';
 import { AddScoreComponent } from './pages/add-score/add-score.component';
 import { MaterialModule } from '../shared/modules/material-module';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [AddScoreComponent],
   imports: [
     CommonModule,
     MatchesRoutingModule,
-    MaterialModule,
-    FlexLayoutModule
+    MaterialModule
   ]
 })
 export class MatchesModule { }
